857753ff663ac74fdcf2ba9976cdf3c1c263ca88
Andy Bell <andy.bell@barkingmad.io>
2021-02-13 20:32:31 +0100

    rename boilerplate code to world and a first pass at a trait to describe this thing

---
+++ pallets/world/src/world.rs
@@ -0,0 +1,32 @@
+
+use frame_support::{
+    dispatch::{result::Result, DispatchError, DispatchResult},
+    traits::Get,
+};
+
+use sp_std::vec::Vec;
+
+pub trait World<AccountId> {
+    
+    type WorldId;
+    type TokenId;
+    type MetaData;
+
+    /// Create a world which is owned by this account
+    fn create(owner: &AccountId) -> Result<Self::WorldId, DispatchError>;
+    /// Mint items for this world, minting 1 would make the item unique
+    fn mint(world_id: &Self::WorldId, amount: u64, data: Option<Self::MetaData>) -> Result<Self::TokenId, DispatchError>;
+    /// Balance of token for user
+    fn balance(world_id: &Self::WorldId, token_id: &Self::TokenId, owner: &AccountId) -> u128;
+    /// Transfer token from owner to new owner
+    fn transfer(world_id: &Self::WorldId, token_id: &Self::TokenId, amount: u64, to: &AccountId) -> Result<(), DispatchError>;
+    /// Transfer multiple tokens
+    fn transfer_multiple(world_id: &Self::WorldId, token_ids: Vec<Self::TokenId>, amount: Vec<u64>, to: Vec<AccountId>) -> Result<(), DispatchError>;
+    /// Approval account for token
+    fn approval(world_id: &Self::WorldId, token_id: &Self::TokenId, approved: &AccountId) -> Result<(), DispatchError>;
+    /// Approval account for multiple tokena
+    fn approval_multiple(world_id: &Self::WorldId, token_ids: Vec<Self::TokenId>, approved: &AccountId) -> Result<(), DispatchError>;
+    /// Burn tokens
+    fn burn(world_id: &Self::WorldId, token_id: &Self::TokenId, amount: u64) -> Result<(), DispatchError>;
+    
+}
