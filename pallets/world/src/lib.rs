648ab6b2aa3aa8ece13fb8822e5b41cd14a21d1c
Andy Bell <andy.bell@barkingmad.io>
2021-02-18 21:34:13 +0100

    mint tokens on creation and store balances

--- pallets/world/src/lib.rs
+++ pallets/world/src/lib.rs
@@ -2,9 +2,11 @@
 use std::fmt::Debug;
 use frame_support::{Parameter, decl_error, decl_event, decl_module, decl_storage, ensure, dispatch::{DispatchError}, traits::Get};
 use frame_system::ensure_signed;
+use sp_core::{hexdisplay::AsBytesRef};
 use sp_runtime::traits::{AtLeast32BitUnsigned, MaybeSerializeDeserialize, Member, CheckedAdd};
 use sp_std::result::Result;
 use codec::{Codec, Encode, Decode};
+use sp_runtime::traits::Hash;
 #[cfg(test)]
 mod mock;
 
@@ -12,6 +14,7 @@ mod mock;
 mod tests;
 
 pub trait Trait: frame_system::Trait {
+	type Balance: Parameter + Member + AtLeast32BitUnsigned + Codec + Default + Copy + MaybeSerializeDeserialize + Debug;
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 	type Identifier: Parameter + Member + AtLeast32BitUnsigned + Codec + Default + Copy + MaybeSerializeDeserialize + Debug + CheckedAdd;
 }
@@ -23,9 +26,9 @@ pub struct Contract {
 }
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
-pub struct Token<AccountId> {
+pub struct Token<AccountId, Balance> {
 	base_uri: Vec<u8>,
-	total_supply: u128,
+	total_supply: Balance,
 	creator: AccountId,
 }
 
@@ -50,7 +53,11 @@ decl_storage! {
 		pub Tokens get(fn tokens):
 			double_map 
 			hasher(blake2_128_concat) T::Identifier, 
-			hasher(blake2_128_concat) T::Identifier => Token<T::AccountId>;
+			hasher(blake2_128_concat) T::Identifier => Token<T::AccountId, T::Balance>;
+
+		pub Balances get(fn balances):
+			map
+			hasher(blake2_128_concat) T::Hash => T::Balance;
 	}
 }
 
@@ -73,6 +80,7 @@ decl_error! {
 		InvalidName,
 		InvalidTotalSupply,
 		InvalidBaseUri,
+		BalanceOverflow,
 	}
 }
 
@@ -97,24 +105,36 @@ decl_module! {
 		}
 
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]		
-		pub fn create_token(origin, contract_id: T::Identifier, creator: T::AccountId, total_supply: u128, base_uri: Vec<u8>) {
+		pub fn create_token(origin, contract_id: T::Identifier, creator: T::AccountId, total_supply: T::Balance, base_uri: Vec<u8>) {
 			let who = ensure_signed(origin)?;
-			ensure!(total_supply > 0, Error::<T>::InvalidTotalSupply);
+			ensure!(total_supply > 0.into(), Error::<T>::InvalidTotalSupply);
 			ensure!(base_uri.len() > 3, Error::<T>::InvalidBaseUri);			
 			ensure!(Contracts::<T>::contains_key(&contract_id), Error::<T>::InvalidContract);
 			ensure!(Owners::<T>::get(&contract_id) == who, Error::<T>::NotContractOwner);
 			let token = Token {
 				base_uri,
 				total_supply,
-				creator,
+				creator: creator.clone(),
 			};
 			let next = Self::get_next_token_id(&contract_id)?;
 			Tokens::<T>::insert(contract_id, next, token);
+			Self::mint(creator, next, total_supply)?;
 		}
 	}
 }
 
 impl <T: Trait> Module<T> {
+	fn mint(to: T::AccountId, token_id: T::Identifier, quantity: T::Balance) -> Result<T::Balance, DispatchError> {
+		let mut encoded = to.encode();
+		encoded.append(&mut token_id.encode());
+		let hash = T::Hashing::hash(encoded.as_bytes_ref());
+		Balances::<T>::try_mutate_exists(hash, |maybe_balance| {
+			let balance = maybe_balance.take().ok_or(Error::<T>::InvalidContract)?;
+			let new_balance = balance.checked_add(&quantity).ok_or(Error::<T>::BalanceOverflow)?;
+			Ok(new_balance)
+		})
+	}
+
 	fn get_next_contract_id() -> Result<T::Identifier, DispatchError> {
 		NextContractId::<T>::try_mutate(|next_id| -> Result<T::Identifier, DispatchError> {
 			let current_id : <T as Trait>::Identifier = *next_id;
@@ -122,6 +142,7 @@ impl <T: Trait> Module<T> {
 			Ok(current_id)
 		})
 	}
+
 	fn get_next_token_id(contract_id: &T::Identifier) -> Result<T::Identifier, DispatchError> {
 		NextTokenId::<T>::try_mutate_exists(contract_id, |maybe_id| {
 			let current_id = maybe_id.take().ok_or(Error::<T>::InvalidContract)?;
