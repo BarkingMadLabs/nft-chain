42584e458478ae758da1a368586ef2975264abc4
Andy Bell <andy.bell@barkingmad.io>
2021-02-19 20:20:31 +0100

    store and mint balances

--- pallets/world/src/lib.rs
+++ pallets/world/src/lib.rs
@@ -2,11 +2,9 @@
 use std::fmt::Debug;
 use frame_support::{Parameter, decl_error, decl_event, decl_module, decl_storage, ensure, dispatch::{DispatchError, DispatchResult}, traits::Get};
 use frame_system::ensure_signed;
-use sp_core::{hexdisplay::AsBytesRef};
 use sp_runtime::traits::{AtLeast32BitUnsigned, Zero, MaybeSerializeDeserialize, Member, CheckedAdd};
 use sp_std::result::Result;
 use codec::{Codec, Encode, Decode};
-use sp_runtime::traits::Hash;
 #[cfg(test)]
 mod mock;
 
@@ -16,14 +14,15 @@ mod tests;
 pub trait Trait: frame_system::Trait {
 	type Balance: Parameter + Member + AtLeast32BitUnsigned + Codec + Default + Copy + MaybeSerializeDeserialize + Debug;
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
-	type Identifier: Parameter + Member + AtLeast32BitUnsigned + Codec + Default + Copy + MaybeSerializeDeserialize + Debug + CheckedAdd;
+	type DomainId: Parameter + Member + AtLeast32BitUnsigned + Codec + Default + Copy + MaybeSerializeDeserialize + Debug + CheckedAdd;
+	type TokenId: Parameter + Member + AtLeast32BitUnsigned + Codec + Default + Copy + MaybeSerializeDeserialize + Debug + CheckedAdd;
 }
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
-pub struct Domain<Identifier, AccountId> {
+pub struct Domain<TokenId, AccountId> {
 	symbol: Vec<u8>,
 	name: Vec<u8>,
-	next_token_id: Identifier,
+	next_token_id: TokenId,
 	owner: AccountId,
 	
 }
@@ -38,22 +37,21 @@ pub struct Token<AccountId, Balance> {
 decl_storage! {
 	trait Store for Module<T: Trait> as Worlds {
 
-		pub NextDomainId get(fn next_domain_id): T::Identifier;
+		pub NextDomainId get(fn next_domain_id): T::DomainId;
 		
 		pub Domains get(fn domains): 
 			map 
-			hasher(blake2_128_concat) T::Identifier => Domain<T::Identifier, T::AccountId>; 
+			hasher(blake2_128_concat) T::DomainId => Domain<T::TokenId, T::AccountId>; 
 		
 		pub Tokens get(fn tokens):
 			double_map 
-			hasher(blake2_128_concat) T::Identifier, 
-			hasher(blake2_128_concat) T::Identifier => Token<T::AccountId, T::Balance>;
+			hasher(blake2_128_concat) T::DomainId,
+			hasher(blake2_128_concat) T::TokenId => Token<T::AccountId, T::Balance>;
 
-		/// Not very user friendly as we need to calculate a hash of 
-		/// account id and token id to get balance
 		pub Balances get(fn balances):
-			map
-			hasher(blake2_128_concat) T::Hash => T::Balance;
+			double_map 
+			hasher(blake2_128_concat) T::AccountId,
+			hasher(blake2_128_concat) (T::DomainId, T::TokenId) => T::Balance;
 	}
 }
 
@@ -61,8 +59,8 @@ decl_event!(
 	pub enum Event<T> 
 	where 
 	AccountId = <T as frame_system::Trait>::AccountId,
-	Identifier = <T as Trait>::Identifier {
-		DomainCreated(AccountId, Identifier),
+	DomainId = <T as Trait>::DomainId {
+		DomainCreated(AccountId, DomainId),
 	}
 );
 
@@ -102,7 +100,7 @@ decl_module! {
 		}
 
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]		
-		pub fn create_token(origin, domain_id: T::Identifier, creator: T::AccountId, total_supply: T::Balance, base_uri: Vec<u8>) -> DispatchResult {
+		pub fn create_token(origin, domain_id: T::DomainId, creator: T::AccountId, total_supply: T::Balance, base_uri: Vec<u8>) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			ensure!(total_supply > 0.into(), Error::<T>::InvalidTotalSupply);
 			ensure!(base_uri.len() > 3, Error::<T>::InvalidBaseUri);			
@@ -114,10 +112,10 @@ decl_module! {
 					total_supply,
 					creator: creator.clone(),
 				};
-				let next = domain.next_token_id.checked_add(&1u32.into()).ok_or(Error::<T>::DomainIdOverflow)?;
-				domain.next_token_id = next;
-				Tokens::<T>::insert(domain_id, next, token);
-				Self::mint(creator, next, total_supply)?;
+				let next_token_id = domain.next_token_id.checked_add(&1u32.into()).ok_or(Error::<T>::DomainIdOverflow)?;
+				domain.next_token_id = next_token_id;
+				Tokens::<T>::insert(domain_id, next_token_id, token);
+				Self::mint(creator, domain_id, next_token_id, total_supply)?;
 				Ok(())
 			})
 		}
@@ -125,20 +123,17 @@ decl_module! {
 }
 
 impl <T: Trait> Module<T> {
-	fn mint(to: T::AccountId, token_id: T::Identifier, quantity: T::Balance) -> Result<T::Balance, DispatchError> {
-		let mut encoded = to.encode();
-		encoded.append(&mut token_id.encode());
-		let hash = T::Hashing::hash(encoded.as_bytes_ref());
-		Balances::<T>::try_mutate_exists(hash, |maybe_balance| {
+	fn mint(to: T::AccountId, domain_id: T::DomainId, token_id: T::TokenId, quantity: T::Balance) -> Result<T::Balance, DispatchError> {
+		Balances::<T>::try_mutate_exists(to, (domain_id, token_id), |maybe_balance| {
 			let balance = maybe_balance.take().ok_or(Error::<T>::InvalidDomain)?;
 			let new_balance = balance.checked_add(&quantity).ok_or(Error::<T>::BalanceOverflow)?;
 			Ok(new_balance)
-		})
+		})	
 	}
 
-	fn get_next_domain_id() -> Result<T::Identifier, DispatchError> {
-		NextDomainId::<T>::try_mutate(|next_id| -> Result<T::Identifier, DispatchError> {
-			let current_id : <T as Trait>::Identifier = *next_id;
+	fn get_next_domain_id() -> Result<T::DomainId, DispatchError> {
+		NextDomainId::<T>::try_mutate(|next_id| -> Result<T::DomainId, DispatchError> {
+			let current_id : <T as Trait>::DomainId = *next_id;
 			*next_id = next_id.checked_add(&0u32.into()).ok_or(Error::<T>::DomainIdOverflow)?;
 			Ok(current_id)
 		})
