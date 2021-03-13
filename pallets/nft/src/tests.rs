223ab2f8a0a6329626f991eea005572589ffe36f
Andy Bell <andy.bell@barkingmad.io>
2021-03-13 13:45:49 +0100

    test failures and check storage when minting a token

--- pallets/nft/src/tests.rs
+++ pallets/nft/src/tests.rs
@@ -11,7 +11,9 @@ const BASE_URI: &str = "https://barkingmad.io/assets";
 #[test]
 fn it_creates_a_domain() {
 	new_test_ext().execute_with(|| {
+		// Confirm we can't create a domain without a valid ticker
 		assert_noop!(NftModule::create_domain(Origin::signed(100), "".as_bytes().to_vec(), NAME.as_bytes().to_vec()), Error::<Test>::InvalidSymbol);
+		// Confirm we can't create a domain without a valid name
 		assert_noop!(NftModule::create_domain(Origin::signed(100), TICKER.as_bytes().to_vec(), "".as_bytes().to_vec()), Error::<Test>::InvalidName);
 		// Create domain ZOD "Zodiac"
 		assert_ok!(NftModule::create_domain(Origin::signed(100), TICKER.as_bytes().to_vec(), NAME.as_bytes().to_vec()));
@@ -27,16 +29,29 @@ fn it_creates_a_domain() {
 #[test]
 fn it_creates_a_token() {
 	new_test_ext().execute_with(|| {
+		const NUM_TOKENS: u64 = 32;
 		// Create domain ZOD "Zodiac"
 		assert_ok!(NftModule::create_domain(Origin::signed(ALICE), TICKER.as_bytes().to_vec(), NAME.as_bytes().to_vec()));
 		// Event for domain creation with owner and domain id
 		assert_eq!(last_event(), Event::nft(RawEvent::DomainCreated(ALICE, 0)));
+		// Confirm we can't create a token without an amount
+		assert_noop!(NftModule::create_token(Origin::signed(ALICE), DOMAIN_ID, BOB, 0, BASE_URI.as_bytes().to_vec()), Error::<Test>::InvalidTotalSupply);		
+		// Confirm we can't create a token without a valid base uri
+		assert_noop!(NftModule::create_token(Origin::signed(ALICE), DOMAIN_ID, BOB, NUM_TOKENS, "".as_bytes().to_vec()), Error::<Test>::InvalidBaseUri);		
+		// Confirm we can't create a token if not domain owner
+		assert_noop!(NftModule::create_token(Origin::signed(BOB), DOMAIN_ID, BOB, NUM_TOKENS, BASE_URI.as_bytes().to_vec()), Error::<Test>::NotDomainOwner);		
+		// Confirm we can't create a token if not a valid domain
+		assert_noop!(NftModule::create_token(Origin::signed(ALICE), 1, BOB, NUM_TOKENS, BASE_URI.as_bytes().to_vec()), Error::<Test>::NotDomainOwner);		
 		// Create token for new domain id, creator being BOB with asset url and mint to BOB 32 tokens
-		assert_ok!(NftModule::create_token(Origin::signed(ALICE), DOMAIN_ID, BOB, 32, BASE_URI.as_bytes().to_vec()));
-		// Event for token creation with id 1 minted 32 for BOB
-		assert_eq!(last_event(), Event::nft(RawEvent::TokenCreated(BOB, DOMAIN_ID, 1, 32)));
+		assert_ok!(NftModule::create_token(Origin::signed(ALICE), DOMAIN_ID, BOB, NUM_TOKENS, BASE_URI.as_bytes().to_vec()));
+		// Check storage for token and supply is correct
+		assert_eq!(NftModule::tokens(DOMAIN_ID, 1).total_supply, NUM_TOKENS);
+		// Check Bob's balance, should be all of them
+		assert_eq!(NftModule::balances(BOB, (DOMAIN_ID, 1)), NUM_TOKENS);
+		// Event for token creation with id 1 minted NUM_TOKENS for BOB
+		assert_eq!(last_event(), Event::nft(RawEvent::TokenCreated(BOB, DOMAIN_ID, 1, NUM_TOKENS)));
 		// Check BOB's balance for Domain id and token id 1, it should be 32
-		assert_eq!(NftModule::balances(BOB, (DOMAIN_ID, 1)), 32);
+		assert_eq!(NftModule::balances(BOB, (DOMAIN_ID, 1)), NUM_TOKENS);
 	});
 }
 
